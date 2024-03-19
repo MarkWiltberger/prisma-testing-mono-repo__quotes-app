75a4cff3ccfd7374670a3916ed44c76d8a0693fd
Mark Wiltberger <mark.wiltberger@gmail.com>
2024-03-19 12:49:06 -0400

    change config to get build to work in Vercel

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,3 +1,3 @@
 import app from 'lib/createServer'
 
-app.listen(3000, () => console.log(`🚀 Server ready at: http://localhost:3000`))
+app.listen(3200, () => console.log(`🚀 Server ready at: http://localhost:3200`))
--- e2e/setenv.ts
+++ e2e/setenv.ts
@@ -1,4 +1,4 @@
 // setenv.ts
 
 import { config } from 'dotenv'
-config({ path: '.env3' })
+config({ path: '.env' })
